518a687798fbef0fb554edbe5cbdbfca2b667f6d
MichaelBrunner <brunner@cadwork.swiss>
2024-06-03 18:49:15 +0200

    ENH: create initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+function App() {
+  const [text, setText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+  const handleSubmit = () => {
+    setSubmittedText(text);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          p: 1,
+          m: 1,
+          borderRadius: 1,
+          bgcolor: "background.paper",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          label="Input"
+          variant="outlined"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          sx={{ m: 1, width: "50ch" }}
+        />
+        <Button variant="contained" onClick={handleSubmit} sx={{ m: 1 }}>
+          Submit
+        </Button>
+        {submittedText && (
+          <Typography variant="h6" component="div" sx={{ color: "black" }}>
+            Submitted Text: {submittedText}
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
