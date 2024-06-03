6f549b0c7559c5c25b92a895afca2641085a7a50
MichaelBrunner <brunner@cadwork.swiss>
2024-06-03 18:56:15 +0200

    ENH: Update App component to use MUI theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
   const [text, setText] = useState("");
@@ -12,49 +13,53 @@ function App() {
     setSubmittedText(text);
   };
 
+  const defaultTheme = createTheme();
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
+    <ThemeProvider theme={defaultTheme}>
       <Box
-        component="form"
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          p: 1,
-          m: 1,
-          borderRadius: 1,
-          bgcolor: "background.paper",
+          height: "100vh",
+          width: "100vw",
         }}
-        noValidate
-        autoComplete="off"
       >
-        <TextField
-          label="Input"
-          variant="outlined"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          sx={{ m: 1, width: "50ch" }}
-        />
-        <Button variant="contained" onClick={handleSubmit} sx={{ m: 1 }}>
-          Submit
-        </Button>
-        {submittedText && (
-          <Typography variant="h6" component="div" sx={{ color: "black" }}>
-            Submitted Text: {submittedText}
-          </Typography>
-        )}
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+            bgcolor: "background.paper",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            label="Input"
+            variant="outlined"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            sx={{ m: 1, width: "50ch" }}
+          />
+          <Button variant="contained" onClick={handleSubmit} sx={{ m: 1 }}>
+            Submit
+          </Button>
+          {submittedText && (
+            <Typography variant="h6" component="div" sx={{ color: "black" }}>
+              Submitted Text: {submittedText}
+            </Typography>
+          )}
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
